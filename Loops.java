7423fb7fe39f786c1cfa8cc143e50a9e7f1c5613
Ayesha Shaikh <ayeshashaikh@Ayeshas-MacBook-Pro.local>
2026-08-10 14:05:43 +0530

    adding Loops.java

---
+++ Loops.java
@@ -0,0 +1,7 @@
+public class Loops {
+    public static void main(String args []) {
+        for (int counter = 0 ; counter < 5 ; counter ++) {
+            System.out.println("Hello World");
+        }
+    }
+}
