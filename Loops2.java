d91ec94b7989740e5c71ae65743b2259da6bcd24
Ayesha Shaikh <ayeshashaikh@Ayeshas-MacBook-Pro.local>
2026-08-10 14:18:25 +0530

    add Loops2.java

---
+++ Loops2.java
@@ -0,0 +1,9 @@
+public class Loops2 {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
