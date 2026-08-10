282a0c344cbc647b201e8ffe0ad5d11ae0deaa16
Ayesha Shaikh <ayeshashaikh@Ayeshas-MacBook-Pro.local>
2026-08-10 15:14:22 +0530

    Add Loops.java5

---
+++ Loops5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Loops5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < 11; i++) {
+
+            System.out.println(i * n);
+
+        }
+    }
+}
