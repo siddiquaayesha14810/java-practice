0787ca29af97b9badbb0dcbe7312bd056a2de91b
Ayesha Shaikh <ayeshashaikh@Ayeshas-MacBook-Pro.local>
2026-08-12 12:10:43 +0530

    Add patterns.java

--- Loops5.java
+++ Loops5.java
@@ -4,7 +4,7 @@ public class Loops5 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 9; i++) {
 
             System.out.println(i * n);
 
