90de8f3b25d1bc9c60aee4436f0e6e4e1362e770
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-16 01:47:56 +0530

    Add Patterns7.java

---
+++ Patterns7.java
@@ -0,0 +1,19 @@
+
+//PYRAMID WITH NUMBERS
+import java.util.*;
+
+public class Patterns7 {
+    public static void main(String args[]) {
+        int n = 5;
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
