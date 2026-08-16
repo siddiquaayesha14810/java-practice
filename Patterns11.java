15832ede86bd6012d5045a3dc6f0cd8abc2a1fe2
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-16 13:30:42 +0530

    Add Patterns11.java

---
+++ Patterns11.java
@@ -0,0 +1,23 @@
+
+//SOLID RHOMBUS
+import java.util.*;
+
+public class Patterns11 {
+    public static void main(String args[]) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
