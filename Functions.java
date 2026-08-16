8cc9baff6e713e1f3cf81eaff2a33435df3f680b
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-16 22:55:00 +0530

    Add functions.java

---
+++ Functions.java
@@ -0,0 +1,18 @@
+
+//PRINT A GIVEN NAME IN A FUNCTION
+import java.util.*;
+
+public class Functions {
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name); // we are calling a function
+    }
+
+}
