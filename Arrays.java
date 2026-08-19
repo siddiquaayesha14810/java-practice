c5334ef1d8cfee19b93e31f8ff3fb721d0013dfc
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-19 19:19:47 +0530

    Add Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+//DEFINING AN ARRAY [1]
+import java.util.*;
+public class Arrays {
+    public static void main (String args[]) {
+        int[] marks = new int [3] ;
+        marks[0] = 97; //phy
+        marks[1] = 98; //chem
+        marks[2] = 95; //eng
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+    
+}
