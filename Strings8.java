da1b0f8b7d3b92cad51e5e7e423b140a4deaf9d2
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-25 13:43:11 +0530

    Add Strings8.java

---
+++ Strings8.java
@@ -0,0 +1,15 @@
+
+// using sb .append() function :
+import java.util.*;
+
+public class Strings8 {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+
+}
