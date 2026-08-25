49b0645b64f8e51543380e0a6862ab35a9b600e1
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-25 13:01:19 +0530

    Add Strings2.java

---
+++ Strings2.java
@@ -0,0 +1,14 @@
+
+//USING CONCATENATION FUNCTION :
+import java.util.*;
+
+public class Strings2 {
+    public static void main(String args[]) {
+        // concatenation
+        String firstName = "tony";
+        String LastName = "stark";
+        String fullName = firstName + " " + LastName;
+        System.out.println(fullName);
+    }
+
+}
