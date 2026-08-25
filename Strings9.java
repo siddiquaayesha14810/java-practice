97e0c75787651044eede9e4001f0fbe6f559d023
Ayesha Siddiqua <siddiquaayesha14810>
2026-08-25 13:51:05 +0530

    Add Strings9.java

---
+++ Strings9.java
@@ -0,0 +1,22 @@
+
+// using reverse function :
+import java.util.*;
+
+public class Strings9 {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("hello");
+
+        for (int i = 0; i < sb.length() / 2; i++) {
+            int front = i;
+            int back = sb.length() - 1 - i; // 5-1-0 ==> 4
+
+            char frontchar = sb.charAt(front);
+            char backchar = sb.charAt(back);
+
+            sb.setCharAt(front, backchar);
+            sb.setCharAt(back, frontchar);
+        }
+        System.out.println(sb);
+    }
+
+}
